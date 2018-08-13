bb36df192872f2cfdaa7901c0c03a3c8c121d1a2
jvelo00 <jamiepopeye@yahoo.com>
2018-08-13 14:40:04 +0800

    add files

---
+++ src/main/java/com/jvelo00/alagaclinicspring/AlagaClinicSpringApplication.java
@@ -0,0 +1,12 @@
+package com.jvelo00.alagaclinicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlagaClinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlagaClinicSpringApplication.class, args);
+    }
+}
